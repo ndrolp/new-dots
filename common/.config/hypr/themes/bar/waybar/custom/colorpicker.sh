8ee8eba7d2bb506b59e622815c9fed96dbd18b5b
Ndrolp <ndro.lozano@gmail.com>
2025-07-02 15:39:14 +0200

    make desktop allways visible and avoid errors on colorpick fail

--- common/.config/hypr/themes/bar/waybar/custom/colorpicker.sh
+++ common/.config/hypr/themes/bar/waybar/custom/colorpicker.sh
@@ -2,6 +2,7 @@
 
 color=$(hyprpicker)
 
-notify-send "Color Picker" "Color ${color} copied to clipboard"
-
-echo "$color" | wl-copy
+if [ -n "$color" ]; then
+    echo "$color" | wl-copy
+    notify-send "Color Picker" "Color ${color} copied to clipboard"
+fi
