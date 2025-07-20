0aaf61d514efca1b8f390008bc377e7327fe88d8
Ndrolp <ndro.lozano@gmail.com>
2025-07-20 18:27:36 +0200

    style rofi and rofi powermenu

---
+++ common/.config/rofi/extras/powermenu/powermenu.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# CMDs
+uptime_info=$(uptime -p | sed -e 's/up //g')
+host=$(hostnamectl hostname)
+
+# Options with Icons and Text
+options=("Balanced" "Power Saver" "Performance")
+icons=("" "󰜉" "")
+
+# Rofi CMD
+rofi_cmd() {
+    options_with_icons=()
+    for ((i = 0; i < ${#options[@]}; i++)); do
+        options_with_icons+=("${icons[$i]}")
+    done
+
+    printf "%s\n" "${options_with_icons[@]}" | \
+    rofi -dmenu -no-config-warnings -i -p " $USER@$host" \
+        -kb-select-1 "p" \
+        -kb-select-2 "r" \
+        -kb-select-3 "l" \
+        -theme ~/.config/rofi/extras/powerstyle/powerstyle.rasi | awk '{print $1}'
+}
+
+# Execute Command
+run_cmd() {
+    echo $1
+    case $1 in
+        "")
+            shutdown now
+            ;;
+        "󰜉")
+            reboot
+            ;;
+        "")
+            hyprctl dispatch exit
+            ;;
+    esac
+}
+
+# Actions
+chosen_option=$(rofi_cmd)
+run_cmd "${chosen_option% *}"
