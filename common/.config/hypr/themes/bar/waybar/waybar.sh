306ef6d7e78f9e14bdf56e9847ab75d3095394e4
Ndrolp <ndro.lozano@gmail.com>
2025-09-30 16:18:41 +0200

    customize swaync

--- common/.config/hypr/themes/bar/waybar/waybar.sh
+++ common/.config/hypr/themes/bar/waybar/waybar.sh
@@ -2,8 +2,11 @@
 
 killall -q waybar
 
-CONFIG="$HOME/.config/hypr/themes/bar/waybar/config.jsonc"
+CONFIG="$HOME/.config/hypr/themes/bar/waybar/waybar.jsonc"
 THEME="$HOME/.config/hypr/themes/bar/waybar/style.css"
 
+
+swaync-client --reload-config
+swaync-client -rs
 waybar --config $CONFIG --style $THEME --log-level debug
 # waybar -c ~/.config/hypr/waybar/config.jsonc & -s ~/.config/hypr/waybar/style.css
