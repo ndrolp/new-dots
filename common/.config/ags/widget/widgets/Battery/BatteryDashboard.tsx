8cc38a3fa580f887a06f8b767ec82469dda1f055
Ndrolp <ndro.lozano@gmail.com>
2026-02-27 02:05:38 +0100

    create structure for battery dashboard

--- common/.config/ags/widget/widgets/Battery/BatteryDashboard.tsx
+++ common/.config/ags/widget/widgets/Battery/BatteryDashboard.tsx
@@ -1,4 +1,4 @@
-import { Astal } from "ags/gtk4"
+import { Astal, Gtk } from "ags/gtk4"
 import app from "ags/gtk4/app"
 
 export const BATTERY_DASHBOARD_WINDOW_NAME = "battery"
@@ -14,10 +14,22 @@ export default function BatteryDashboard() {
       exclusivity={Astal.Exclusivity.EXCLUSIVE}
       class={"Battery catppuccin"}
       anchor={TOP | RIGHT}
+      marginRight={10}
+      margin_top={10}
       application={app}
     >
-      <box class="container">
-        <label label="Dashboard" />
+      <box class="container" orientation={Gtk.Orientation.VERTICAL}>
+        <label
+          class="title"
+          label="󰁹  Battery Info"
+          halign={Gtk.Align.CENTER}
+          hexpand
+          margin_bottom={10}
+        />
+        <box class="battery-dashboard-percentage" hexpand>
+          <slider hexpand value={0.5} min={0} max={1} />
+          <label label="50%" />
+        </box>
       </box>
     </window>
   )
